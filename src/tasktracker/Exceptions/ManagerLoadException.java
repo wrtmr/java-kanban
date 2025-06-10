8999bd6ae976a44dfae90fabc3bc1de95896eea6
Wartimerz <danryzhkov@mail.ru>
2025-06-10 19:44:05 +0300

    - Fixed Exceptions style errors

--- src/tasktracker/Exceptions/ManagerLoadException.java
+++ src/tasktracker/Exceptions/ManagerLoadException.java
@@ -1,6 +1,7 @@
 package tasktracker.Exceptions;
 
 public class ManagerLoadException extends RuntimeException {
+
     public ManagerLoadException(String message) {
         super(message);
     }
--- src/tasktracker/Exceptions/ManagerSaveException.java
+++ src/tasktracker/Exceptions/ManagerSaveException.java
@@ -2,11 +2,11 @@ package tasktracker.Exceptions;
 
 public class ManagerSaveException extends RuntimeException {
 
-    public ManagerSaveException (String message) {
+    public ManagerSaveException(String message) {
         super(message);
     }
 
-    public ManagerSaveException (String message, Throwable cause) {
+    public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
 }
